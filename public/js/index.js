129a3156be2bb1d5d971e23c9a4a7ca494464489
Karan Wadhwa <wadhwa.karan96@gmail.com>
2017-08-19 00:41:38 +0530

    used materializecss & cleaned up the UI

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: 0.85, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+  });
+  $('#modal1').on('click', function() {
+  });
+});
+
+
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+        
